93e9a9b457de58b118449f809cdc038f25e6bcd7
Bob Sacamano <sacamano@twititytwat.com>
2017-06-20 12:33:33 +0000

    Add some documentation. Save data to .rds.

---
+++ R/applications.R
@@ -0,0 +1,8 @@
+#' Assylum applications
+#' 
+#' Monthly data from the Swedish Migration Agency.
+#' 
+#' @format A data frame with 12 variables: \code{year_month}, \code{applications},
+#' \code{male}, \code{female}, \code{minors}, \code{unacc_minors}, \code{decisions},
+#' \code{granted}, \code{rejected}, \code{dublin}, \code{others}, \code{avg_time}
+"applications"
